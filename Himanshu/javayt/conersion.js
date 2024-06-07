769bc6536f63185e27549468629e6758a89ccb59
Himanshu <hv271005@gmail.com>
2024-06-07 23:45:28 +0530

    First Commit

---
+++ Himanshu/javayt/conersion.js
@@ -0,0 +1,23 @@
+let score = 33;
+console.log(typeof(score))
+
+let scr = "273"
+let num = Number(scr)
+console.log(typeof(num))
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1;false = 0;
+
+let isLoggedIn = 1;
+let a = Boolean(isLoggedIn)
+console.log(typeof(a))
+
+//"" => false
+//"Hitesh"=>true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof(stringNumber))
